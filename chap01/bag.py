cc77ba594e0dcb245051aa243f62ce8895313441
김찬희 <blackeagle10@icloud.com>
2025-04-13 17:42:34 +0900

    Print bag list with print_list()
    clean up other print()

--- chap01/bag.py
+++ chap01/bag.py
@@ -21,6 +21,18 @@ def num_of(bag, item):
     return bag.count(item)
 
 
+def print_list(message: str, bag: list):
+    print(message, end='')
+
+    for i in range(len(bag)):
+        print(bag[i], end='')
+
+        if i is not len(bag) - 1:
+            print(', ', end='')
+
+    print('\n')
+
+
 def main():
     my_bag: list = []
 
@@ -31,18 +43,19 @@ def main():
     insert(my_bag, 'handkerchief')
     insert(my_bag, 'comb')
     insert(my_bag, 'book')
-    insert(my_bag, 'baseball')
+    insert(my_bag, 'baseball ball')
 
-    print('Items in the bag:', my_bag)
-    print(f'Duration of the first task: {(time.time() - start) * 1000:.6f} ms')
-    print('')
+    print_list('Items in the bag: ', my_bag)
+    print(f'Duration of the first task: {(time.time() - start) * 1000:.6f} ms\n')
+
+    print('-' * 50 + '\n')
 
     insert(my_bag, 'comb')
     remove(my_bag, 'handkerchief')
 
-    print('Items in the bag:', my_bag)
-    print(f'Duration of the second task: {(time.time() - start) * 1000:.6f} ms')
-    print('')
+    print_list('Items in the bag: ', my_bag)
+    print(f'Duration of the second task: {(time.time() - start) * 1000:.6f} ms\n')
+    print('-' * 50 + '\n')
 
     target_item = input('Enter the name of the item to count: ')
     print(f'{target_item}: {num_of(my_bag, target_item)} item(s)')
