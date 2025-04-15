85c2a2ab5ed63c372c6823b00c23b645dd9f141d
김찬희 <blackeagle10@icloud.com>
2025-04-15 11:27:29 +0900

    chap 2 practice turn string to upper and lower case

--- chap02/list_practice.py
+++ chap02/list_practice.py
@@ -27,9 +27,46 @@ def print_list_reverse() -> None:
         print(f'{theList[i]}')
 
 
+# 2.8
+# ord(char) : turn char into unicode integer
+# chr(integer) : tun integer into character
+def string_upper_lower() -> None:
+    msg: str = 'Data Structure in Python'
+
+    def to_upper_case(msg: str) -> str:
+        upper_msg: str = ''
+
+        for i in range(len(msg)):
+            c = msg[i]
+            if 'a' <= c <= 'z':
+                upper_msg += chr(ord(c) - 32)
+            else:
+                upper_msg += c
+
+        return upper_msg
+
+    def to_lower_case(msg) -> str:
+        lower_msg = ''
+
+        for i in range(len(msg)):
+            c = msg[i]
+            if 'A' <= c <= 'Z':
+                lower_msg += chr(ord(c) + 32)
+            else:
+                lower_msg += c
+
+        return lower_msg
+
+    print(f'Original message: {msg}')
+    print('-' * 50)
+    print(f'Message in upper case: {to_upper_case(msg)}')
+    print(f'Message in lower case: {to_lower_case(msg)}')
+
+
 def main() -> None:
     # print_list_reverse()
-    celsius_fahrenheit()
+    # celsius_fahrenheit()
+    string_upper_lower()
 
 
 if __name__ == '__main__':
