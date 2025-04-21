2d1aaf37c4ece05b211cec5ccd97e33d353e8c8f
김찬희 <blackeagle10@icloud.com>
2025-04-21 15:13:25 +0900

    Chap03 finish line_editor.py practice
    and add replace() member method in ArrayList.py

--- chap03/ArrayList.py
+++ chap03/ArrayList.py
@@ -1,5 +1,5 @@
 class ArrayList:
-    def __int__(self, capacity=100):
+    def __init__(self, capacity=100):
         self.capacity = capacity
         self.array = [None] * capacity
         self.size = 0
@@ -11,7 +11,7 @@ class ArrayList:
         return self.size == self.capacity
 
     def get_entry(self, pos):
-        if 0 <= pos <= self.size:
+        if 0 <= pos < self.size:
             return self.array[pos]
         else:
             return None
@@ -22,19 +22,19 @@ class ArrayList:
                 self.array[i] = self.array[i - 1]
             self.array[pos] = e
             self.size += 1
-        else:
-            pass
+
+    def replace(self, pos, e):
+        if 0 <= pos < self.size:
+            self.array[pos] = e
 
     def delete(self, pos):
-        if not self.is_empty() and 0 <= pos <= self.size:
+        if not self.is_empty() and 0 <= pos < self.size:
             e = self.array[pos]
             for i in range(pos, self.size - 1):
                 self.array[i] = self.array[i + 1]
+            self.array[self.size - 1] = None
             self.size -= 1
             return e
-        else:
-            pass
 
-
-def __str__(self):
-    return str(self.array[0 : self.size])
+    def __str__(self):
+        return str(self.array[: self.size])
