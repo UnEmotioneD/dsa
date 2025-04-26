65a25ad9a4703fae0259b92804fd535aa0826e2e
UnEmotioneD <blackeagle10@icloud.com>
2025-04-26 20:32:52 +0900

    chap01 factorial with iteration and recursion

---
+++ chap01/factorial.py
@@ -0,0 +1,35 @@
+"""
+Chapter 01
+Page 37
+
+---
+
+iteration and recursive
+"""
+
+
+def factorial_iter(user_input: int) -> int:
+    result: int = 1
+
+    for i in range(1, user_input + 1):
+        result = result * i
+
+    return result
+
+
+def factorial_recursion(number: int) -> int:
+    if number == 1:
+        return 1
+    else:
+        return number * factorial_recursion(number - 1)
+
+
+def main() -> None:
+    user_input = int(input('Integer to factorial: '))
+    print('-' * 40)
+    print(f'Iteration: {factorial_iter(user_input)}')
+    print(f'Resucsion: {factorial_recursion(user_input)}')
+
+
+if __name__ == '__main__':
+    main()
