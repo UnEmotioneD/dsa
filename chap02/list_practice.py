16272840d9b6a91383fe9d51109af0a5fe44280b
UnEmotioneD <blackeagle10@icloud.com>
2025-04-27 20:23:22 +0900

    chap02 list practice string_upper_lower method
    range -> enumerate
    
    add emumerate comment
    
    yeah

--- chap02/list_practice.py
+++ chap02/list_practice.py
@@ -28,15 +28,16 @@ def print_list_reverse() -> None:
 
 
 # 2.8
-# ord(char) : turn char into unicode integer
-# chr(integer) : tun integer into character
+# ord(char): turn char into unicode integer
+# chr(integer): tun integer into character
+# enumerate: iterate and loop at the same time
 def string_upper_lower() -> None:
     msg: str = 'Data Structure in Python'
 
     def to_upper_case(msg_to_upper: str) -> str:
         upper_msg: str = ''
 
-        for i in range(len(msg_to_upper)):
+        for i, c in enumerate(msg_to_upper):
             c = msg_to_upper[i]
             if 'a' <= c <= 'z':
                 upper_msg += chr(ord(c) - 32)
@@ -48,7 +49,7 @@ def string_upper_lower() -> None:
     def to_lower_case(msg_to_lower) -> str:
         lower_msg = ''
 
-        for i in range(len(msg_to_lower)):
+        for i, c in enumerate(msg_to_lower):
             c = msg_to_lower[i]
             if 'A' <= c <= 'Z':
                 lower_msg += chr(ord(c) + 32)
