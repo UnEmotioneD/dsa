7ff9123c5742f9d20d4ba9ebac88a2b53e13c0e4
UnEmotioneD <blackeagle10@icloud.com>
2025-05-18 00:28:12 +0900

    Chap04: ArrayStack, infix_to_postfix done

---
+++ chap04/ArrayStack.py
@@ -0,0 +1,29 @@
+class ArrayStack:
+    def __init__(self, capacity):
+        self._capacity = capacity
+        self._array = [None] * capacity
+        self._top = -1
+
+    def is_empty(self):
+        return self._top == -1
+
+    def is_full(self):
+        return self._top == self._capacity - 1
+
+    def push(self, item):
+        if self.is_full():
+            raise IndexError('Pusning to full stack')
+        self._top += 1
+        self._array[self._top] = item
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('Popping from empty stack')
+        item = self._array[self._top]
+        self._top -= 1
+        return item
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('Peeking from empty stack')
+        return self._array[self._top]
--- chap04/infix_to_postfix.py
+++ chap04/infix_to_postfix.py
@@ -1,17 +1,21 @@
 """
 19102467 김찬희
 
+chapter 04
 Page 150
----
-example)
-input: 8/2-3+(3*2)
-output: 82/3-32*+
 """
 
-def check_brackets(statement):
-    stack = ArrayStack(100)
-    for ch in statement:
-        if ch == '(' or ch == '{' or ch == '[':
+from ArrayStack import ArrayStack
+
+
+def precedence(op):
+    if op in ('(', ')'):
+        return 0
+    if op in ('+', '-'):
+        return 1
+    if op in ('*', '/'):
+        return 2
+    return -1
 
 
 def infix_2_postfix(expr):
@@ -19,12 +23,69 @@ def infix_2_postfix(expr):
     output = []
 
     for term in expr:
-        if term in '(':
-            s.push('()')
+        if term == '(':
+            s.push(term)
+        elif term == ')':
+            while not s.is_empty():
+                op = s.pop()
+                if op == '(':
+                    break
+                output.append(op)
+        elif term in ('+', '-', '*', '/'):
+            while not s.is_empty():
+                op = s.peek()
+                if precedence(term) <= precedence(op):
+                    output.append(s.pop())
+                else:
+                    break
+            s.push(term)
+        else:
+            output.append(term)
+
+    while not s.is_empty():
+        output.append(s.pop())
+
+    return output
+
+
+def eval_post_fix(expr):
+    s = ArrayStack(100)
+
+    for token in expr:
+        if token in '+-*/':
+            val2 = s.pop()
+            val1 = s.pop()
+            if token == '+':
+                s.push(val1 + val2)
+            elif token == '-':
+                s.push(val1 - val2)
+            elif token == '*':
+                s.push(val1 * val2)
+            elif token == '/':
+                s.push(val1 / val2)
+        else:
+            s.push(float(token))
+
+    return s.pop()
 
 
 def main() -> None:
-    print('Hello, world')
+    infix1 = ['8', '/', '2', '-', '3', '+', '(', '3', '*', '2', ')']
+    infix2 = ['1', '/', '2', '*', '4', '*', '(', '1', '/', '4', ')']
+
+    postfix1 = infix_2_postfix(infix1)
+    postfix2 = infix_2_postfix(infix2)
+
+    result1 = eval_post_fix(postfix1)
+    result2 = eval_post_fix(postfix2)
+
+    print(f'중위표기: {infix1}')
+    print(f'후위표기: {postfix1}')
+    print(f'계산 결과: {result1}')
+    print()
+    print(f'중위표기: {" ".join(infix2)}')
+    print(f'후위표기: {" ".join(postfix2)}')
+    print(f'계산 결과: {result2}')
 
 
 if __name__ == '__main__':
