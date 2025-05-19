b401a02cdf2ff222c039f84b651b1c8a95d3c783
UnEmotioneD <blackeagle10@icloud.com>
2025-05-19 17:59:54 +0900

    Chap05 run dequeue from main.py

--- chap05/main.py
+++ chap05/main.py
@@ -6,6 +6,7 @@ Execute circular queue
 """
 
 
+from circular_dequeue import CircularDequeue
 from circular_queue import CircularQueue
 
 
@@ -31,8 +32,36 @@ def run_cricular_queue():
     print(f'\tG H I inserted {que}')
 
 
+def run_cricular_dequeue():
+    dq = CircularDequeue()
+
+    for i in range(9):
+        if i % 2 == 0:
+            dq.add_rear(i)
+        else:
+            dq.add_front(i)
+
+    print(f'Odd number -> front, Even number -> rear: {dq}')
+
+    dq.delete_front()
+    dq.delete_front()
+
+    dq.delete_rear()
+    dq.delete_rear()
+    dq.delete_rear()
+
+    print(f'delete front twice and rear 3 times: {dq}')
+
+    for i in range(9, 14):
+        dq.add_front(i)
+
+    print(f'add front 9 to 13: {dq}')
+
+
 def main() -> None:
     run_cricular_queue()
+    print('\n', '=' * 70, '\n')
+    run_cricular_dequeue()
 
 
 if __name__ == '__main__':
