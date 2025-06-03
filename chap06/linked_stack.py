097e8fa14840c19ff4926656e94fae60a05296d4
UnEmotioneD <blackeagle10@icloud.com>
2025-06-03 20:35:15 +0900

    Chap06 linked_stack - add method size, __str__

--- chap06/linked_stack.py
+++ chap06/linked_stack.py
@@ -8,13 +8,13 @@ class LinkedStack:
     def __init__(self):
         self.top = None
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.top is None
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return False
 
-    def push(self, e):
+    def push(self, e) -> None:
         self.top = Node(e, self.top)
 
     def pop(self):
@@ -26,3 +26,19 @@ class LinkedStack:
     def peek(self):
         if not self.is_empty():
             return self.top.data
+
+    def size(self) -> int:
+        node = self.top
+        count: int = 0
+        while not node is None:
+            node = node.link
+            count += 1
+        return count
+
+    def __str__(self):
+        arr = []
+        node = self.top
+        while not node is None:
+            arr.append(node.data)
+            node = node.link
+        return str(arr)
