ef67a0e6dc35d34ab9462dde775648065eccfdbc
UnEmotioneD <blackeagle10@icloud.com>
2025-06-04 13:52:50 +0900

    Add Chap06 main.py

---
+++ chap06/main.py
@@ -0,0 +1,44 @@
+"""
+19102467 김찬희
+"""
+
+
+from linked_stack import LinkedStack
+
+
+def run_linked_stack() -> None:
+    stack = LinkedStack()
+
+    print(f'Is stack empty? {stack.is_empty()}')
+
+    stack.push(1)
+    print('push 1')
+
+    stack.push(2)
+    print('push 2')
+
+    stack.push(3)
+    print('push 3')
+
+    print('Inside the stack: ', end='')
+    print(stack)
+
+    print(f'Is stack empty? {stack.is_empty()}')
+
+    print(f'size of linked_stack: {stack.size()}')
+
+    print(f'peek: {stack.peek()}')
+    print(f'pop: {stack.pop()}')
+
+    print('Inside the stack: ', end='')
+    print(stack)
+
+    print(f'pop: {stack.pop()}')
+    print(f'pop: {stack.pop()}')
+    print(f'pop: {stack.pop()}')
+
+    print(stack)
+
+
+if __name__ == '__main__':
+    run_linked_stack()
