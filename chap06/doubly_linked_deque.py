171300c7e07467384f02a5018bc3d1d9c51515a0
UnEmotioneD <blackeagle10@icloud.com>
2025-06-10 22:45:54 +0900

    Add: chap06 DNode, DoublyLinkedDequeue. no methods yet

---
+++ chap06/doubly_linked_deque.py
@@ -0,0 +1,32 @@
+class DNode:
+    def __init__(self, elem, prev=None, next=None):
+        self.data = elem
+        self.prev = prev
+        self.next = next
+
+
+class DoublyLinkedDequeue:
+    def __init__(self, front=None, rear=None):
+        self.front = front
+        self.rear = rear
+
+    def is_empty(self) -> bool:
+        return self.front == self.rear
+
+    def add_front(self) -> None:
+        return None
+
+    def add_rear(self) -> None:
+        return None
+
+    def delete_front(self):
+        return None
+
+    def delete_rear(self):
+        return None
+
+    def size(self) -> int:
+        return 0
+
+    def __str__(self):
+        return ''
