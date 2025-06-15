26a218f078ff10e10743f45a5989f11e9d2909a0
UnEmotioneD <blackeagle10@icloud.com>
2025-06-16 07:42:13 +0900

    Fix: chap04 circular_dequeu add_front() method

--- chap05/circular_deque.py
+++ chap05/circular_deque.py
@@ -17,21 +17,22 @@ class CircularDeque(CircularQueue):
 
     # New methods
     def add_front(self, item):
-        if not self.is_full():
-            item = self.array[self.front] = item
-            self.front = (self.front - 1 + self.capacity) % self.capacity
-        else:
-            pass
+        if self.is_full():
+            raise IndexError('Deque if full.')
+        # front points at the empty slot by default
+        #  add the new item where the front is pointing
+        self.array[self.front] = item
+        #  then move the front forward
+        self.front = (self.front - 1 + self.capacity) % self.capacity
 
     def delete_rear(self):
         if self.is_empty():
-            raise IndexError('Queue is empty')
-
+            raise IndexError('Queue is empty.')
         item = self.array[self.rear]
         self.rear = (self.rear - 1 + self.capacity) % self.capacity
         return item
 
     def get_rear(self):
         if self.is_empty():
-            raise IndexError('Queue is empty')
+            raise IndexError('Queue is empty.')
         return self.array[self.rear]
