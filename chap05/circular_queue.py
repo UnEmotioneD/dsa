5464d571bc86f22b11191eae622e45259b5d300e
UnEmotioneD <blackeagle10@icloud.com>
2025-06-16 07:37:40 +0900

    Update: chap04 raise IndexError if empty

--- chap05/circular_queue.py
+++ chap05/circular_queue.py
@@ -9,25 +9,28 @@ class CircularQueue:
         return self.front == self.rear
 
     def is_full(self) -> bool:
+        # if front and rear is side by side
         return self.front == (self.rear + 1) % self.capacity
 
     def enqueue(self, item) -> None:
         if self.is_full():
-            return
+            raise IndexError('Queue is Full.')
+        # move rear backward
+        #  if rear == capacity move is to start
         self.rear = (self.rear + 1) % self.capacity
         self.array[self.rear] = item
 
     def dequeue(self):
         if self.is_empty():
-            raise IndexError('Queue is empty')
-
+            raise IndexError('Queue is empty.')
+        # move front backward
+        #  since it is pointing at the empty slot front of the element
         self.front = (self.front + 1) % self.capacity
         return self.array[self.front]
 
     def peek(self):
         if self.is_empty():
-            raise IndexError('Queue is empty')
-
+            raise IndexError('Queue is empty.')
         return self.array[(self.front + 1) % self.capacity]
 
     def size(self):
@@ -36,12 +39,12 @@ class CircularQueue:
     def __str__(self) -> str:
         if self.is_empty():
             return '[]'
-
         if self.front < self.rear:
             return str(self.array[self.front + 1 : self.rear + 1])
-
-        result = (
+        # if front is pointing back of rear
+        #  front to the end
+        #  then index 0 to the rear
+        return str(
             self.array[self.front + 1 : self.capacity]
             + self.array[0 : self.rear + 1]
         )
-        return str(result)
