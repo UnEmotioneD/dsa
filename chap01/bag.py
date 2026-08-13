656f12da3adad4b47d2b2925aeea302e4eeb3c2e
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-08-13 16:58:25 +0900

    format: chap01 w/ ruff & add types

--- chap01/bag.py
+++ chap01/bag.py
@@ -1,66 +1,66 @@
 import time
 
 
-def contains(bag, item):
+def contains(bag: list[str], item: str) -> bool:
     return item in bag
 
 
-def insert(bag, item):
+def insert(bag: list[str], item: str) -> None:
     bag.append(item)
 
 
-def remove(bag, item):
+def remove(bag: list[str], item: str) -> None:
     bag.remove(item)
 
 
-def count(bag):
+def count(bag: list[str]) -> int:
     return len(bag)
 
 
-def num_of(bag, item):
+def num_of(bag: list[str], item: str) -> int:
     return bag.count(item)
 
 
-def print_list(message: str, bag: list):
-    print(message, end="")
+def print_list(message: str, bag: list[str]) -> None:
+    print(message, end='')
 
     for i in range(len(bag)):
-        print(bag[i], end="")
+        print(bag[i], end='')
 
         if i is not len(bag) - 1:
-            print(", ", end="")
+            print(', ', end='')
 
-    print("\n")
+    print('\n')
 
 
 def main():
-    my_bag: list = []
+    my_bag: list[str] = []
 
     start = time.time()
 
-    insert(my_bag, "phone")
-    insert(my_bag, "wallet")
-    insert(my_bag, "handkerchief")
-    insert(my_bag, "comb")
-    insert(my_bag, "book")
-    insert(my_bag, "baseball ball")
+    insert(my_bag, 'phone')
+    insert(my_bag, 'wallet')
+    insert(my_bag, 'handkerchief')
+    insert(my_bag, 'comb')
+    insert(my_bag, 'book')
+    insert(my_bag, 'baseball ball')
 
-    print_list("Items in the bag: ", my_bag)
-    print(f"Duration of the first task: {(time.time() - start) * 1000:.6f} ms\n")
+    print_list('Items in the bag: ', my_bag)
+    print(f'Duration of the first task: {(time.time() - start) * 1000:.6f} ms\n')
 
-    print("-" * 50 + "\n")
+    print('-' * 50 + '\n')
 
-    insert(my_bag, "comb")
-    remove(my_bag, "handkerchief")
+    insert(my_bag, 'comb')
+    remove(my_bag, 'handkerchief')
 
-    print_list("Items in the bag: ", my_bag)
-    print(f"Duration of the second task: {(time.time() - start) * 1000:.6f} ms\n")
-    print("-" * 50 + "\n")
+    print_list('Items in the bag: ', my_bag)
+    print(f'Duration of the second task: {(time.time() - start) * 1000:.6f} ms\n')
+    print('-' * 50 + '\n')
 
-    target_item = input("Enter the name of the item to count: ")
-    print(f"{target_item}: {num_of(my_bag, target_item)} item(s)")
-    print(f"Total duration: {(time.time() - start):.6f} sec")
+    target_item = input('Enter the name of the item to count: ')
+    print(f'{target_item}: {num_of(my_bag, target_item)} item(s)')
+    print(f'Total duration: {(time.time() - start):.6f} sec')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- chap01/hanoi_tower.py
+++ chap01/hanoi_tower.py
@@ -1,9 +1,9 @@
 def hanoi_tower(n: int, fr: str, temp: str, to: str) -> None:
     if n == 1:
-        print(f"원판 1: {fr} --> {to}")
+        print(f'원판 1: {fr} --> {to}')
     else:
         hanoi_tower(n - 1, fr, to, temp)
-        print(f"원판 {n}: {fr} --> {to}")
+        print(f'원판 {n}: {fr} --> {to}')
         hanoi_tower(n - 1, temp, fr, to)
 
 
@@ -11,23 +11,23 @@ def main() -> None:
     number_of_plate: int = 0
 
     while True:
-        print("=" * 30)
+        print('=' * 30)
 
         try:
-            number_of_plate = int(input("원판의 갯수를 입력하시오: "))
+            number_of_plate = int(input('원판의 갯수를 입력하시오: '))
             if number_of_plate <= 0:
-                print("원판을 추가 하시오.")
+                print('원판을 추가 하시오.')
                 continue
             break
 
         except ValueError:
-            print("정수를 입력하시오.")
+            print('정수를 입력하시오.')
 
-    first_pole: str = "A"
-    second_pole: str = "B"
-    third_pole: str = "C"
+    first_pole: str = 'A'
+    second_pole: str = 'B'
+    third_pole: str = 'C'
     hanoi_tower(number_of_plate, first_pole, second_pole, third_pole)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
