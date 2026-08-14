711fc0e1daad8cc0fbe0551cef7264c5f7a364db
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-08-14 12:10:53 +0900

    feat: btree - get user input and search

--- chap08/btree.py
+++ chap08/btree.py
@@ -5,12 +5,11 @@ TODO:
     - [x] create and shuffle the list
     - [x] create binary tree
     - [x] sort with in_order traverse
-    - [ ] get user input to search and delete
-    - [ ] on search print each node it goes through
+    - [x] get user input to search and delete
+    - [x] on search print each node it goes through
+    - [ ] node deletion
     - [ ] print binary tree in tree style
 """
-
-import random
 
 
 class Node:
@@ -38,6 +37,39 @@ def in_order(node: Node | None, res: list[int]) -> None:
     in_order(node.right, res)
 
 
+def get_search_no(numbers: list[int]) -> int:
+    low: int = min(numbers)
+    high: int = max(numbers)
+
+    while True:
+        try:
+            search_no: int = int(input(f'Enter number to search [{low} ~ {high}]: '))
+
+            if search_no >= low and search_no <= high:
+                break
+        except ValueError:
+            print('Input integer type', end='\n\n')
+
+    return search_no
+
+
+def search_node(node: Node | None, search_no: int) -> None:
+    if node is None:
+        print('Not found')
+        return
+
+    if search_no is node.data:
+        print(f'Reached: {node.data}')
+        return
+    else:
+        print(f'Traversing: {node.data}')
+
+    if search_no < node.data:
+        search_node(node.left, search_no)
+    else:
+        search_node(node.right, search_no)
+
+
 def _insert(root: Node | None, data: int) -> Node:
     if root is None:
         return Node(data)
@@ -51,9 +83,9 @@ def _insert(root: Node | None, data: int) -> Node:
 
 
 def main() -> None:
-    numbers: list[int] = list[int](range(10))
-    random.shuffle(numbers)
-    # numbers: list[int] = [5, 8, 7, 0, 4, 9, 3, 2, 6, 1]  # fixed order for testing
+    # numbers: list[int] = list[int](range(10))
+    # random.shuffle(numbers)
+    numbers: list[int] = [5, 8, 7, 0, 4, 9, 3, 2, 6, 1]  # fixed order for testing
     res: list[int] = []
 
     print('Before sorted: ')
@@ -65,6 +97,9 @@ def main() -> None:
 
     print('After in_order: ')
     print(res, end='\n\n')
+
+    search_no: int = get_search_no(numbers)
+    search_node(root, search_no)
 
 
 if __name__ == '__main__':
